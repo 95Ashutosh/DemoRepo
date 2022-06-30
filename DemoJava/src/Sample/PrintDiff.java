ee50960fa82ac7312948114f2a9fba250f57caea
ashutosh-jha <jha.ashutosh95@gmail.com>
2022-06-30 18:19:54 +0530

    first commit

---
+++ DemoJava/src/Sample/PrintDiff.java
@@ -0,0 +1,10 @@
+package Sample;
+
+public class PrintDiff {
+public void Diff()
+{
+	int a =10;
+	int b=5;
+	System.out.println(a-b);
+}
+}
---
+++ DemoJava/src/Sample/PrintSum.java
@@ -0,0 +1,9 @@
+package Sample;
+
+public class PrintSum {
+	public void Sum() {
+		int a = 5;
+		int b = 10;
+System.out.println(a+b);
+	}
+}
